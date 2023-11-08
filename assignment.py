232c50000a06809082821838f645563c06557e40
MateuszR643 <144241927+MateuszR643@users.noreply.github.com>
2023-11-08 05:46:55 +0000

    Updated assignment

--- assignment.py
+++ assignment.py
@@ -5,19 +5,18 @@ size = input("Please input the number of the menu item you are ordering. ")
 toppings = input("How many toppings do you want? \n 0 - $0.00\n 1 - $1.00 \n 2 - $1.75 \n 3 - $2.50 \n 4 - $3.35 \n")
 pizzaprices = [6.00, 10.00]
 topprices = [0.00, 1.00, 1.75, 2.50, 3.35]
-price = 0.00
 
 def calculateprice():
     tax = pizzaprices[size-1]*0.13 + topprices[toppings]*0.13
-    price += pizzaprices[size-1] + topprices[toppings] + tax
-    print("You ordered:", size, "and", toppings, "toppings \n You're total is: $" + str(price), "\n Thank you for ordering from Dezmond's Pizzeria!")
+    price = round(pizzaprices[size-1] + topprices[toppings] + tax, 2)
+    print("You ordered: item", size, "and", toppings, "toppings \n Your total is: $" + str(price), "\n Thank you for ordering from Dezmond's Pizzeria!")
 
 if size == "1" or size == "2":
     size = int(size)
-    if toppings == "0" or toppings == "1" or toppings == "2" or toppings == "3" toppings == "4":
+    if toppings == "0" or toppings == "1" or toppings == "2" or toppings == "3" or toppings == "4":
         toppings = int(toppings)
         calculateprice()
     else:
         print("Input not in range of topppings.")
 else:
-    print("Please input a numerical value from the menu")
+    print("Please input a numerical value from the menu.")
--- handler.php
+++ handler.php
@@ -1,21 +1,40 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
   <head>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="512x512" href="favicons/android-chrome-512x512.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="favicons/android-chrome-192x192.png">
+    <link rel="manifest" href="favicons/site.webmanifest">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
     <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-    <title>Welcome Page</title>
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Dezmond's Pizzeria</title>
   </head>
 
-  <body>
-    
+  <body class="body">
+    <h1 class="header">Dezmond's Pizzeria</h1>
+
     <?php
-    $myVariable = "";
-    if ( isset( $_POST['myVariable'] ) ){
-      $myVariable = $_POST['myVariable'];
+    $size = "";
+    $toppings = "";
+    $pizzaprices = [6.00, 10.00];
+    $topprices = [0.00, 1.00, 1.75, 2.50, 3.35];
+
+    if ( isset( $_POST['size']) and isset( $_POST['toppings'])) {
+      $myVariable = $_POST['size'];
+      $myVariable = $_POST['toppings'];
+      $tax = $pizzaprices[$size - 1] * 0.13 + $topprices[$topprices - 0] * 0.13;
+      $price = $pizzaprices[$size - 1] + $topprices[$topprices - 0] + $tax;
+      echo "<p class=\"paragraph\">You ordered item ".$size." and ".$toppings." toppings
+      \n Your total is: $".$price.
+      "\n Thank you for ordering from Dezmond's Pizzeria</p>";
     }
-    echo "<h1>My Program</h1>\n";
-    echo "<p>My Variable is = ".$myVariable."</p>\n";
+    else {
+      echo "<h1 class=\"header\">Please input a complete order.</h1>";
+    }
     ?>
     
   </body>
